cd63edc9fa153220d78c660949211ce7749caa99
lightlitebug <light.lite.bug@gmail.com>
2022-12-03 15:08:45 +0900

    SplashPage

--- lib/pages/splash/splash_page.dart
+++ lib/pages/splash/splash_page.dart
@@ -1,4 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import '../../repositories/auth_repository.dart';
+import '../auth/signin_page.dart';
+import '../auth/signup_page.dart';
+import '../auth/verify_email_page.dart';
 
 class SplashPage extends StatelessWidget {
   static const String routeName = '/';
@@ -6,10 +12,32 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: CircularProgressIndicator(),
-      ),
+    return StreamBuilder<User?>(
+      stream: AuthRepository.userStream,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        User? user = snapshot.data;
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return const Scaffold(
+            body: Center(
+              child: Text(
+                'Firebase Connection Error\n\nTry later!',
+                style: TextStyle(fontSize: 18.0, color: Colors.red),
+              ),
+            ),
+          );
+        } else if (user != null) {
+          return const VerifyEmailPage();
+        } else {
+          return const SignupPage();
+        }
+      },
     );
   }
 }
